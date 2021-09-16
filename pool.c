fbd62baee0584a2d1a1e826c1461687dcb5282f7
IAmFunkyFrog <st085873@student.spbu.ru>
2021-09-16 14:48:49 +0300

    Pool allocator first implementation

--- pool.c
+++ pool.c
@@ -4,11 +4,27 @@
 #include "pool.h"
 
 void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+    *p = POOL_INITIALIZER(p, nmemb, membsz);
 }
 
 void *pool_alloc(struct pool *p) {
+    if(p->free != NULL) {
+        struct pool_free_block *fb = p->free;
+        p->free = fb->next;
+        return fb;
+    }
+    else if(p->freestart < p->freeend) {
+        void *b = p->freestart;
+        p->freestart += p->membsz;
+        return b;
+    }
 	return NULL;
 }
 
 void pool_free(struct pool *p, void *ptr) {
+    if(ptr >= p->mem && ptr <= p->freeend) {
+        struct pool_free_block *newb = ptr;
+        *newb = (struct pool_free_block){.next = p->free};
+        p->free = newb;
+    }
 }

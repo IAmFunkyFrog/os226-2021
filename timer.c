b2cb2b10f4f4d05f17a905f016ad310844f31904
IAmFunkyFrog <st085873@student.spbu.ru>
2021-10-15 15:21:44 +0300

    Implementation of timer.c

--- timer.c
+++ timer.c
@@ -11,9 +11,23 @@
 #include "timer.h"
 
 int timer_cnt(void) {
-	// TODO: getitimer
+	struct itimerval curr_value;
+    getitimer(ITIMER_PROF, &curr_value);
+    return curr_value.it_value.tv_usec + curr_value.it_value.tv_sec * 1000000;
 }
 
 void timer_init(int ms, void (*hnd)(void)) {
-	// TODO: setitimer
+    struct timeval interval = {
+            .tv_sec = (ms * 1000) / 1000000,
+            .tv_usec = (ms * 1000) % 1000000
+    };
+    struct itimerval new_value = {
+            .it_interval = interval,
+            .it_value = interval
+    };
+    if(signal(SIGPROF, hnd) == SIG_ERR) {
+        fprintf(stderr, "Unable to set handler on signal in timer_init()");
+        exit(-1);
+    }
+    setitimer(ITIMER_PROF, &new_value, NULL);
 }
